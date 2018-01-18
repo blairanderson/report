2d73534180da1e119c652cfe8d7d4fa338875510
Blair Anderson <Blair81@gmail.com>
2018-01-18 09:26:51 -0800

    update links and add tags

--- src/components/NicheLinks/index.js
+++ src/components/NicheLinks/index.js
@@ -6,9 +6,22 @@ const P = ({ children }) => (
 );
 
 var LINKS = {
-  "SaaS -  Ebitda VS Net Profit ": "https://www.saasmetrics.co/ebitda-vs-gross-margin-vs-net-profit/",
-  "AMAZON - FBA Revenue Calculator": "https://sellercentral.amazon.com/hz/fba/profitabilitycalculator/index?lang=en_US",
-  "ART - Becoming a Profitable Artist": "https://www.forbes.com/sites/rachelhennessey/2013/04/02/3-keys-to-making-it-as-an-artist-without-starving/"
+  "Ebitda VS Net Profit": {
+    url: "https://www.saasmetrics.co/ebitda-vs-gross-margin-vs-net-profit/",
+    tags: "SAAS"
+  },
+  "Revenue Calculator": {
+    url: "https://sellercentral.amazon.com/hz/fba/profitabilitycalculator/index?lang=en_US",
+    tags: "FBA"
+  },
+  "Becoming a Profitable Artist": {
+    url: "https://www.forbes.com/sites/rachelhennessey/2013/04/02/3-keys-to-making-it-as-an-artist-without-starving/",
+    tags: "ART"
+  },
+  "People leave brands in a DownTurn Economy": {
+    url: "http://www.tuck.dartmouth.edu/news/articles/private-label-products-in-the-manufacturer-retailer-power-balance",
+    tags: "PL"
+  }
 }
 
 
@@ -18,7 +31,8 @@ const NicheLinks = () => (
   <div>
     <small>Links currated by Niche</small>
     {Object.keys(LINKS).sort().map((name) => {
-      return (<P key={name}><a className="no-underline" href={LINKS[name]}>{name}</a></P>);
+      var {url, tags} = LINKS[name]
+      return (<P key={name}><a className="no-underline" href={url}>{tags}{" - "}{name}</a></P>);
     })}
   </div>
 )
